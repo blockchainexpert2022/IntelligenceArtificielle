935c1d7bf79971058ca0c554d018cf61a9529a68
blockchainexpert2022 <114223263+blockchainexpert2022@users.noreply.github.com>
2025-08-26 14:59:51 +0200

    Create langchain_gemini.py

---
+++ googlecolab/langchain_gemini.py
@@ -0,0 +1,16 @@
+!pip install -qU "langchain[google-genai]"
+
+#AIzaSyBifMdAQ2kT5IgunZZVU-52k-sJK9wYCuA
+
+import getpass
+import os
+
+if not os.environ.get("GOOGLE_API_KEY"):
+  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+
+model.invoke("as-tu accès à internet ?")
+
